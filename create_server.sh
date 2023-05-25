0cb4d7407b53c196f0fedd179b69f582dfc6b539
rilan0r <omargadzhiev.islam@gmail.com>
2023-05-25 16:23:47 +0300

    typo in IP address detection

--- create_server.sh
+++ create_server.sh
@@ -1,5 +1,5 @@
 apt update
-apt install openvpn easy-rsa net-tools -y
+apt install openvpn easy-rsa -y
 mkdir /etc/openvpn/easy-rsa
 cp -R /usr/share/easy-rsa /etc/openvpn
 cd /etc/openvpn/easy-rsa/
@@ -39,8 +39,8 @@ mute 10
 explicit-exit-notify 1
 --push \"redirect-gateway\"" >> /etc/openvpn/server.conf
 
-var1=$(route | grep '^default' | grep -o '[^ ]*$')
-var2=$(ip addr | grep $var4 | grep inet | awk '{print $2}' | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
+var1=$(ip route | grep '^default' | awk '{print $5}')
+var2=$(ip addr | grep $var1 | grep inet | awk '{print $2}' | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)")
 var3=$(cat /etc/openvpn/ca.crt)
 var4=$(cat /etc/openvpn/ta.key)
 echo "client
